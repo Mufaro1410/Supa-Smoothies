13c36ce274ab63730896e7918b00f9389d5c043e
Mufaro1410 <fungurayimufaro@gmail.com>
2024-12-16 14:15:26 +0200

    added date fetching functionality from supabase and display on frontend

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,42 @@
+import { useEffect, useState } from "react"
 import supabase from "../supabaseClient"
+import SmoothieCard from "../components/SmoothieCard"
 
 export default function Home() {
+    const [fetchError, setFetchError] = useState(null)
+    const [smoothies, setSmoothies] = useState(null)
+
+    useEffect(() => {
+        const fetchSmoothies = async () => {
+            const {data, error} = await supabase.from("smoothies").select()
+
+            if (error) {
+                setFetchError("Could not fetch the smoothies")
+                setSmoothies(null)
+                console.log(error);
+            }
+            if (data) {
+                setSmoothies(data)
+                setFetchError(null)
+            }
+        }
+
+        fetchSmoothies()
+
+    }, [])
+
   return (
     <div className="page home">
-        <h2>Home</h2>
+        {fetchError && <p>{fetchError}</p>}
+        {smoothies && (
+            <div className="smoothies">
+                <div className="smooothie-grid">
+                {smoothies.map(smoothie => (
+                    <SmoothieCard key={smoothie.id} smoothie={smoothie}/>
+                ))}
+                </div>
+            </div>
+        )}
     </div>
   )
 }
